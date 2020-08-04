4f45cbe036b986e7f58deddbfdb90a458fd657a3
Megan <megan.findley@live.co.uk>
2020-08-04 10:56:57 +0100

    login check update

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,44 +1,125 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Redirect } from "react-router-dom";
 
 function Question() {
-    const [questions, setQuestions] = useState({
-        q1: '',
-        q1Ans: '',
-        q2: '',
-        q2Ans: '',
-        q3: '',
-        q3Ans: ''
+    
+    // ---- logged in info --------------------
+    const [loggedin, setLoggedin] = useState({
+        login: false,
+        loading: true
     });
-
     useEffect(() => {
         getApi();
-        console.log("Page Loaded");
-    }, [])
-
+    }, []);
     const getApi = async () => {
-        const res = await axios.get('https://opentdb.com/api.php?amount=3');
+        const res = await axios.get('/hidden');
+        setLoggedin({
+            login: res.data.loggin,
+            loading: false
+        });
+    }
+    //--------------------------------------------
+
+    const [redirect, setRedirect] =useState({
+        redirect: false
+    });
+    console.log(redirect);
+
+    const [quizDetails, setQuizDetails] = useState({
+        amount: '10',
+        category: '9',
+        difficulty: 'easy'
+    });
+
+    const setData = (e) => {
+        setQuizDetails({
+            ...quizDetails,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+
+        const body = JSON.stringify({
+            amount: quizDetails.amount,
+            category: quizDetails.category,
+            difficulty: quizDetails.difficulty
+        });
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.post("/questions", body, config);
         console.log(res.data);
 
-        setQuestions({
-            q1: res.data.results[0].question,
-            q1Ans: res.data.results[0].correct_answer,
-            q2: res.data.results[1].question,
-            q2Ans: res.data.results[1].correct_answer,
-            q3: res.data.results[2].question,
-            q3Ans: res.data.results[2].correct_answer
+        setRedirect({
+            redirect: true
         })
-        console.log(questions);
     }
+
+    // -----Log in check-----------------------
+    if (!loggedin.login && !loggedin.loading) {
+        console.log('inside of redirect')
+        return <Redirect to='/login' />
+    }
+    else if(redirect.redirect){
+        return <Redirect to={{
+            pathname:'/questions',
+            state: {
+                amount: quizDetails.amount, 
+                category: quizDetails.category,
+                difficulty: quizDetails.difficulty
+             }
+        }}/>
+    }
+    //------------------------------------------
     return (
         <div>
-            <h1>Trivia Questions Examples</h1>
-            <h3>{questions.q1}</h3>
-            <h3>{questions.q1Ans}</h3>
-            <h3>{questions.q2}</h3>
-            <h3>{questions.q2Ans}</h3>
-            <h3>{questions.q3}</h3>
-            <h3>{questions.q3Ans}</h3>
+            <h1>Trivia Quiz Set Up</h1>
+            <h2>Only Visable to logged in users</h2>
+            <form className="quizForm">
+                <label>No. of Questions: </label>
+                <input type='number' name='amount'defaultValue='10' min='5' max='50' onChange={setData}/>
+                <label>Categories: </label>
+                <select name='category' onChange={setData}>
+                    <option defaultValue="9" >General Knowledge</option>
+                    <option value="10">Books</option>
+                    <option value="11">Film</option>
+                    <option value="12">Music</option>
+                    <option value="13">Musicals & Theatre</option>
+                    <option value="14">TV</option>
+                    <option value="15">Video Games</option>
+                    <option value="16">Board Games</option>
+                    <option value="17">Science & Nature</option>
+                    <option value="18">Computers</option>
+                    <option value="19">Mathematics</option>
+                    <option value="20">Mythology</option>
+                    <option value="21">Sports</option>
+                    <option value="22">Geography</option>
+                    <option value="23">History</option>
+                    <option value="24">Politics</option>
+                    <option value="25">Art</option>
+                    <option value="26">Celebrities</option>
+                    <option value="27">Animals</option>
+                    <option value="28">Vehicles</option>
+                    <option value="29">Comics</option>
+                    <option value="30">Gadgets</option>
+                    <option value="31">Anime & Manga</option>
+                    <option value="32">Cartoon & Animations</option>
+                </select>
+                <label>Difficulty: </label>
+                <select name='difficulty' onChange={setData}>
+                    <option defaultValue="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+                <button type='submit' onClick={submitForm}>Make Quiz</button>
+            </form>
         </div>
     )
 }
--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import {Questionaire} from './Main';
-
+import React, { useState, useEffect } from 'react';
+import { Questionaire } from './Main';
+import axios from 'axios';
+import { Redirect } from "react-router-dom";
 import './quiz.css'
 
 const API_URL = 'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple'
@@ -8,23 +9,23 @@ const API_URL = 'https://opentdb.com/api.php?amount=5&category=9&difficulty=medi
 function Quiz() {
     const [questions, setQuestions] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [score, setScore] =useState(0);
+    const [score, setScore] = useState(0);
     const [showAnswers, setShowAnswers] = useState(false);
-    
+
     useEffect(() => {
         fetch(API_URL)
-        .then((res) => res.json())
-        .then((data) => {
-            setQuestions(data.results);
-        });
+            .then((res) => res.json())
+            .then((data) => {
+                setQuestions(data.results);
+            });
     }, []);
 
     const handleAnswer = (answer) => {
-        const newIndex = currentIndex +1
+        const newIndex = currentIndex + 1
         setCurrentIndex(newIndex);
 
-        if(answer === questions[currentIndex].
-        correct_answer){
+        if (answer === questions[currentIndex].
+            correct_answer) {
             setScore(score + 1);
         }
 
@@ -33,25 +34,45 @@ function Quiz() {
         // const newIndex = currentIndex +1
         // setCurrentIndex(newIndex);
     };
-
-        return questions.length > 0 ? (
-            <div className="container">
-                {currentIndex >= questions.length ? (
+     // ---- logged in info --------------------
+     const [loggedin, setLoggedin] = useState({
+        login: false,
+        loading: true
+    });
+    useEffect(() => {
+        getApi();
+    }, []);
+    const getApi = async () => {
+        const res = await axios.get('/hidden');
+        setLoggedin({
+            login: res.data.loggin,
+            loading: false
+        });
+    }
+    //--------------------------------------------
+    if (!loggedin.login && !loggedin.loading) {
+        console.log('inside of redirect')
+        return <Redirect to='/login' />
+    }
+    //--------------------------------------------
+    return questions.length > 0 ? (
+        <div className="container">
+            {currentIndex >= questions.length ? (
                 <h1 className="">
                     Game ended! Your score is: {score}.
                 </h1>
             ) : (
-                <Questionaire className="container"
-                data={questions[currentIndex]}
-                showAnswers={showAnswers} 
-                handleAnswer={handleAnswer} 
-                />
-            )}                
-        </div>     
-        ) : (
+                    <Questionaire className="container"
+                        data={questions[currentIndex]}
+                        showAnswers={showAnswers}
+                        handleAnswer={handleAnswer}
+                    />
+                )}
+        </div>
+    ) : (
             <h2 class="container">Just loading...!</h2>
         );
-        
-    }
+
+}
 
 export default Quiz;
